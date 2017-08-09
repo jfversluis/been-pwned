614da5cc6600152586e2ff84b5548be84221b15c
Gerald Versluis <gerald@verslu.is>
2017-08-09 11:59:31 +0200

    Initial solution structure

---
+++ src/BeenPwned.App/iOS/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+namespace BeenPwned.App.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+
+            LoadApplication(new BeenPwnedApp());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
