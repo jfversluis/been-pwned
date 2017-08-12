cde39ada14c95048632f58b817a78b0b3294facb
sthewissen <sthewissen@kembit.nl>
2017-08-12 20:40:24 +0200

    Added webview for the description text

---
+++ src/BeenPwnedApp/iOS/Renderers/ExtendedWebViewRenderer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using BeenPwned.App.Core.Controls;
+using BeenPwned.App.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedWebView), typeof(ExtendedWebViewRenderer))]
+namespace BeenPwned.App.iOS.Renderers
+{
+    public class ExtendedWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            // Bugfix to get rid of the black line at the bottom.
+            this.Opaque = false;
+            this.BackgroundColor = UIColor.Clear;
+
+            this.ScrollView.ScrollEnabled = false;
+            this.ScrollView.Bounces = false;
+
+            Delegate = new ExtendedUIWebViewDelegate(this);
+        }
+    }
+
+    public class ExtendedUIWebViewDelegate : UIWebViewDelegate
+    {
+        ExtendedWebViewRenderer webViewRenderer;
+
+        public ExtendedUIWebViewDelegate(ExtendedWebViewRenderer _webViewRenderer = null)
+        {
+            webViewRenderer = _webViewRenderer ?? new ExtendedWebViewRenderer();
+        }
+
+        public override bool ShouldStartLoad(UIWebView webView, NSUrlRequest request, UIWebViewNavigationType navigationType)
+        {
+            if (navigationType == UIWebViewNavigationType.LinkClicked)
+            {
+                UIApplication.SharedApplication.OpenUrl(request.Url);
+                return false;
+            }
+
+            return true;
+        }
+
+        public override async void LoadingFinished(UIWebView webView)
+        {
+            // TODO: Find out why this doesn't work as it should.
+            var wv = webViewRenderer.Element as ExtendedWebView;
+            if (wv != null)
+            {
+                await System.Threading.Tasks.Task.Delay(100); // wait here till content is rendered
+                wv.HeightRequest = (double)webView.ScrollView.ContentSize.Height;
+            }
+        }
+    }
+}
