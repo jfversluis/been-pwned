38bbf3260a3b5e234c9037151ade786a4e62c79a
Gerald Versluis <gerald@verslu.is>
2017-08-13 20:47:00 +0200

    Abstracted caching to Service class
    Moved caching to the Service class so we won’t have to worry about it
    in the PageModels. Also updated BeenPwned.Api package to the latest
    version

--- src/BeenPwnedApp/Core/PageModels/BreachesPageModel.cs
+++ src/BeenPwnedApp/Core/PageModels/BreachesPageModel.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Akavache;
 using BeenPwned.Api.Models;
 using BeenPwned.App.Core.Services;
 using MvvmHelpers;
@@ -15,26 +13,26 @@ namespace BeenPwned.App.Core.PageModels
     {
         bool _isNavigating;
 
-		private readonly ObservableRangeCollection<Breach> _breaches = new ObservableRangeCollection<Breach>();
+        private readonly ObservableRangeCollection<Breach> _breaches = new ObservableRangeCollection<Breach>();
         public ObservableCollection<Breach> Breaches { get { return _breaches; } }
 
         public ICommand OpenBreachCommand => new Command(async (item) => await OpenBreach(item), (arg) => !_isNavigating);
 
-		private ICommand _refreshCommand;
-		public ICommand RefreshCommand
-		{
-			get
-			{
-				return _refreshCommand ??
-					(_refreshCommand = new Command(ExecuteRefreshCommand));
-			}
-		}
+        private ICommand _refreshCommand;
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return _refreshCommand ??
+                    (_refreshCommand = new Command(ExecuteRefreshCommand));
+            }
+        }
 
         protected override void ViewIsAppearing(object sender, EventArgs e)
         {
             base.ViewIsAppearing(sender, e);
 
-            GetBreaches();
+            ExecuteRefreshCommand();
         }
 
         public async Task OpenBreach(object breach)
@@ -46,34 +44,17 @@ namespace BeenPwned.App.Core.PageModels
             _isNavigating = false;
         }
 
-		public void GetBreaches(bool force = false)
-		{
-			var cache = BlobCache.LocalMachine;
-            cache.GetAndFetchLatest("breaches", GetRemoteBreaches,
-				offset =>
-				{
-					TimeSpan elapsed = DateTimeOffset.Now - offset;
-					var invalidate = (force || elapsed > new TimeSpan(24, 0, 0));
-					return invalidate;
-				})
-				.Subscribe((contributions) =>
-				{
-                    _breaches.ReplaceRange(contributions);
-				});
-		}
-
-        private async Task<IEnumerable<Breach>> GetRemoteBreaches()
-		{
-            return await BeenPwnedService.Instance.GetAllBreaches();
-		}
-
-		private void ExecuteRefreshCommand()
-		{
+        private void ExecuteRefreshCommand()
+        {
             IsLoading = true;
 
-            GetBreaches(true);
+            BeenPwnedService.Instance.GetAllBreaches()
+                            .Subscribe((contributions) =>
+                {
+                    _breaches.ReplaceRange(contributions);
+                });
 
             IsLoading = false;
-		}
+        }
     }
 }
--- src/BeenPwnedApp/Core/Services/BeenPwnedService.cs
+++ src/BeenPwnedApp/Core/Services/BeenPwnedService.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using Akavache;
 using BeenPwned.Api;
+using BeenPwned.Api.Models;
 using Xamarin.Forms;
 
 namespace BeenPwned.App.Core.Services
 {
     internal class BeenPwnedService
     {
-        private static Lazy<BeenPwnedClient> _pwnedClient = new Lazy<BeenPwnedClient>(() => new BeenPwnedClient($"BeenPwned-{Device.RuntimePlatform}"));
-        internal static BeenPwnedClient Instance => _pwnedClient.Value;
+        private IBeenPwnedClient _pwnedClient = new BeenPwnedClient($"BeenPwned-{Device.RuntimePlatform}");
+		private static Lazy<BeenPwnedService> _pwnedService = new Lazy<BeenPwnedService>(() => new BeenPwnedService());
+        internal static BeenPwnedService Instance => _pwnedService.Value;
+
+        internal IObservable<IEnumerable<Breach>> GetAllBreaches(bool force = false)
+        {
+			var cache = BlobCache.LocalMachine;
+            return cache.GetAndFetchLatest("breaches", async () => await _pwnedClient.GetAllBreaches(),
+                offset =>
+                {
+                    TimeSpan elapsed = DateTimeOffset.Now - offset;
+                    var invalidate = (force || elapsed > new TimeSpan(24, 0, 0));
+                    return invalidate;
+                });
+        }
     }
 }
