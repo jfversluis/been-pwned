7c2835a83f85139540981a661beeb66459bb30ab
Gerald Versluis <gerald@verslu.is>
2017-08-17 09:16:46 +0200

    Implemented hiding password value in searchbar
    Like the website; havibeenpwned.com the searchbar for entering a
    password value is now masked.
    
    It requires a bit of a hack, because it doesn’t have support for a
    password mask out of the box

---
+++ src/BeenPwnedApp/Core/Converters/StringLengthToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace BeenPwned.App.Core.Converters
+{
+    public class StringLengthToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var length = System.Convert.ToInt32(value);
+
+            if (length > 0)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
