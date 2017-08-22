5e7f27d26573a41eeadce8eeac19a317eebe8f67
Gerald Versluis <gerald@verslu.is>
2017-08-22 10:22:06 +0200

    Added OSS libraries, FAQ and link to app GitHub
    Added some body to the SettingsPage.

--- src/BeenPwnedApp/Core/PageModels/SettingsPageModel.cs
+++ src/BeenPwnedApp/Core/PageModels/SettingsPageModel.cs
@@ -1,9 +1,19 @@
-﻿namespace BeenPwned.App.Core.PageModels
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BeenPwned.App.Core.PageModels
 {
     public class SettingsPageModel : BasePageModel
     {
-        public SettingsPageModel()
+		private ICommand _openUrlCommand;
+		public ICommand OpenUrlCommand => _openUrlCommand ?? (_openUrlCommand = new Command<string>(async (url) => await OpenUrl(url), (arg) => !_isNavigating));
+
+        private async Task OpenUrl(string url)
         {
+            // TODO implement with IBrowserService from other branch
+            Device.OpenUri(new Uri(url));
         }
-    }
+	}
 }
