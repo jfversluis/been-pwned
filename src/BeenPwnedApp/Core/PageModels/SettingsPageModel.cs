ab84c6d1d5fdf8754d73007d64b4519c3b813af6
Gerald Versluis <gerald@verslu.is>
2017-08-22 13:44:28 +0200

    Added push notifications settings page
    Also updated Lottie packages to make animations work again. Logic for
    actually enabling push notifications still remains to be done.

--- src/BeenPwnedApp/Core/PageModels/SettingsPageModel.cs
+++ src/BeenPwnedApp/Core/PageModels/SettingsPageModel.cs
@@ -10,10 +10,22 @@ namespace BeenPwned.App.Core.PageModels
 		private ICommand _openUrlCommand;
 		public ICommand OpenUrlCommand => _openUrlCommand ?? (_openUrlCommand = new Command<string>(async (url) => await OpenUrl(url), (arg) => !_isNavigating));
 
+		private ICommand _openPushSettingsCommand;
+		public ICommand OpenPushSettingsCommand => _openPushSettingsCommand ?? (_openPushSettingsCommand = new Command<string>(async (o) => await OpenPushSettings(), (arg) => !_isNavigating));
+
         private async Task OpenUrl(string url)
         {
             // TODO implement with IBrowserService from other branch
             Device.OpenUri(new Uri(url));
+        }
+
+        private async Task OpenPushSettings()
+        {
+            _isNavigating = true;
+
+            await CoreMethods.PushPageModel<PushNotificationsPageModel>();
+
+            _isNavigating = false;
         }
 	}
 }
